fd4676088f4200b97f34483e23d0f8f9ad580e1f
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-05-25 19:42:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Array of API URLs to fetch data from
 const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/1",
   "https://jsonplaceholder.typicode.com/todos/2",
@@ -12,4 +11,69 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
 
-// You can write your code here
+function fetchDataWithPromiseAll(apiUrls) {
+  const startTime = new Date().getTime();
+
+  const promises = apiUrls.map((url) => fetch(url));
+
+  return Promise.all(promises)
+    .then(() => {
+      const endTime = new Date().getTime();
+      const timeTaken = endTime - startTime;
+      return timeTaken;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      return -1;
+    });
+}
+
+function fetchDataWithPromiseAny(apiUrls) {
+  const startTime = new Date().getTime();
+
+  const promises = apiUrls.map((url) => fetch(url));
+
+  return Promise.any(promises)
+    .then(() => {
+      const endTime = new Date().getTime();
+      const timeTaken = endTime - startTime;
+      return timeTaken;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      return -1;
+    });
+}
+
+// Displaying results on the webpage
+const table = document.getElementById("results-table");
+
+// Function to add a row to the table
+function addRow(method, timeTaken) {
+  const row = table.insertRow();
+  const methodCell = row.insertCell();
+  const timeTakenCell = row.insertCell();
+
+  methodCell.textContent = method;
+  timeTakenCell.textContent = timeTaken;
+}
+
+// Fetch data using Promise.all and display the time taken
+fetchDataWithPromiseAll(apiUrls)
+  .then((timeTaken) => {
+    addRow("Promise.all", timeTaken);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+    addRow("Promise.all", "Error");
+  });
+
+// Fetch data using Promise.any and display the time taken
+fetchDataWithPromiseAny(apiUrls)
+  .then((timeTaken) => {
+    addRow("Promise.any", timeTaken);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+    addRow("Promise.any", "Error");
+  });
